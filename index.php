6afd556fd56a91ff08b82edf2edcc395d478bf4f
YukiSaito-1st <12kaikei@gmail.com>
2020-08-22 13:21:03 +0900

    ペットショップアプリ課題2

--- index.php
+++ index.php
@@ -14,23 +14,75 @@ try {
     exit;
 }
 
-// SQL文の組み立て
-$sql = 'SELECT * FROM animals';
+?>
 
-// プリペアドステートメントの準備
-// $dbh->query($sql) でも良い
-$stmt = $dbh->prepare($sql);
+<!DOCTYPE html>
+<html>
 
-// プリペアドステートメントの実行
-$stmt->execute();
+<head>
+    <meta charset="utf-8">
+    <title>pet_shop</title>
+</head>
 
-// 結果の受け取り
-$animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+<body>
+    <h2>本日のご紹介ペット!</h2>
+    <form action="" method="get">
+        <div>
+            <label for="keyword">キーワード:</label>
+            <input type="text" name="keyword" placeholder="キーワードの入力">
+            <input type="submit" value="検索">
+        </div>
+        <br>
+    </form>
+    <?php if ($_GET["keyword"] === "") : ?>
+        <?php
+        // SQL文の組み立て
+        $sql = 'SELECT * FROM animals';
 
-foreach ($animals as $animal) {
-    echo $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん。<br>';
-    echo $animal['description'] . '<br>';
-    echo $animal['birthday'] . '生まれ<br>';
-    echo '出身地' . $animal['birthplace'] . '<br>';
-    echo '<hr>';
-}
+        // プリペアドステートメントの準備
+        // $dbh->query($sql) でも良い
+        $stmt = $dbh->prepare($sql);
+        
+        // プリペアドステートメントの実行
+        $stmt->execute();
+
+        // 結果の受け取り
+        $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+        <?php foreach ($animals as $animal) : ?>
+            <?= $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん。' ?><br>
+            <?= $animal['description'] ?><br>
+            <?= $animal['birthday'] . ' 生まれ' ?><br>
+            <?= '出身地 ' . $animal['birthplace'] ?>
+            <hr>
+        <?php endforeach; ?>
+    <?php else : ?>
+        <?php
+        $keyword = $_GET["keyword"];
+        $keyword = '%' . $keyword . '%'; // %ではさむ
+
+        // SQL文の組み立て
+        $sql = "SELECT * FROM animals WHERE description LIKE :keyword";
+
+        // プリペアドステートメントの準備
+        // $dbh->query($sql) でも良い
+        $stmt = $dbh->prepare($sql);
+
+        // プリペアドステートメントの実行
+        $stmt->bindValue(":keyword", $keyword, PDO::PARAM_STR);
+        $stmt->execute();
+
+        // 結果の受け取り
+        $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+        <?php foreach ($animals as $animal) : ?>
+            <?= $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん。' ?><br>
+            <?= $animal['description'] ?><br>
+            <?= $animal['birthday'] . ' 生まれ' ?><br>
+            <?= '出身地 ' . $animal['birthplace'] ?>
+            <hr>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</body>
+
+</html>
